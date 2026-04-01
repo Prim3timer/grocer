fea2f5ed1e021e05472f0e9cbb1d6234aed90506
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-04-01 16:30:03 +0100

    return link

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_URL;
-// const BASE_URL = "https://ukuta.onrender.com";
+// const BASE_URL = process.env.REACT_APP_URL;
+const BASE_URL = "https://ukuta.onrender.com";
 
 export default axios.create({
   baseURL: BASE_URL,
